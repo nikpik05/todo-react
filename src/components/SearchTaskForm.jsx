bcfd493e3e1b261ff79b6541db675c4abd200ad6
Nikita Pikulin <pikulinnik05@yandex.ru>
2026-09-13 01:53:30 +0300

    добавлен функционал кнопкам

--- src/components/SearchTaskForm.jsx
+++ src/components/SearchTaskForm.jsx
@@ -6,7 +6,7 @@ const SearchTaskForm = (props) => {
   } = props;
 
   return (
-    <form className="todo__form">
+    <form className="todo__form" onSubmit={(event) => event.preventDefault()}>
         <Field
           className="todo__field"
           label="Search task"
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import AddTaskForm from "./AddTaskForm"
 import SearchTaskForm from "./SearchTaskForm"
 import TodoInfo from "./TodoInfo"
@@ -5,32 +6,65 @@ import TodoList from "./TodoList"
 
 
 const Todo = () => {
-const tasks = [
+
+const [tasks, setTasks] = useState([
   {id: "task-1", title: "погладить кота", isDone: true},
   {id: "task-2", title: "погладить собаку", isDone: false},
   {id: "task-3", title: "погладить попугая", isDone: true}
-]
+]);
+
+const [newTaskTitle, setNewTaskTitle] = useState('');
 
 const deleteAllTasks = () => {
-  console.log('удаляем все задачи');
+  const isConfirmed = confirm('Are you sure want to delete all?')
+
+  if(isConfirmed){
+    setTasks([])
+  }
 }
 
 const deleteTask = (taskId) => {
-  console.log('удаляем задачу')
+  setTasks(
+    tasks.filter((task) => task.id !== taskId)
+  )
 }
 
 const ToggleTaskComplete = (taskId, isDone) => {
-  console.log(`задача ${taskId} ${isDone ? 'выполнена': 'не выполнена'}`)
+  setTasks(
+    tasks.map((task) => {
+      if(task.id === taskId){
+        return {...task, isDone}
+      }
+      return task
+    })
+  )
 }
 
 const filterTask = (query) => {
   console.log(`поиск: ${query}`)
 }
 
+const addTask = () => {
+  if(newTaskTitle.trim().length > 0){
+    const newTask = {
+      id: crypto?.randomUUID() ?? Date.now().toString(),
+      title:newTaskTitle,
+      isDone: false,
+    }
+
+    setTasks([...tasks, newTask])
+    setNewTaskTitle('')
+  }
+}
+
   return(
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm/>
+      <AddTaskForm
+        addTask={addTask}
+        newTaskTitle={newTaskTitle}
+        setNewTaskTitle={setNewTaskTitle}
+      />
       <SearchTaskForm
         onSearchInput={filterTask}
       />
